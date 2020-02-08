44bbb9ea4e21efba151cde0b5699b636e8e4f035
yihur <jialidediannao@qq.com>
2020-02-08 16:43:28 +0800

    modify birthday welfare

--- src/main/java/com/company/wages/calculation/system/type/EmployeeTypeService.java
+++ src/main/java/com/company/wages/calculation/system/type/EmployeeTypeService.java
@@ -2,11 +2,15 @@ package com.company.wages.calculation.system.type;
 
 import com.company.wages.calculation.system.domain.Employee;
 
+/**
+ * @author hy
+ */
 public interface EmployeeTypeService {
 
     /**
-     * 计算员工类型方法
+     * 根据不同胡员工类型计算员工薪资
      * @param month
+     * @param employeeType
      * @return
      */
     double employeeTypeWageCalculation(String month, Employee employeeType);
